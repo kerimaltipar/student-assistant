21a10127329a3c16cbbd42499f8c124af7bf17d9
unknown <kerimaltipar@gmail.com>
2019-12-28 00:03:29 +0300

    gecme notu hesapla activity added

---
+++ app/src/main/java/com/example/studentassistant/GecmeNotuHesaplaActivity.java
@@ -0,0 +1,14 @@
+package com.example.studentassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GecmeNotuHesaplaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gecme_notu_hesapla);
+    }
+}
